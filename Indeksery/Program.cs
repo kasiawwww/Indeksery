7932430dccf8b65d32e9133d83e2eda25148947e
test <k.widlak@microsoft.wsei.edu.pl>
2019-02-07 18:07:31 +0100

    zmianki

--- Indeksery/Program.cs
+++ Indeksery/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IndexersClasses;
 
 namespace Indeksery
 {
@@ -10,6 +11,16 @@ namespace Indeksery
     {
         static void Main(string[] args)
         {
+            Names names = new Names(5);
+            names[0] = "Roman";
+            names[1] = "Aśka";
+            names[2] = "Krzyś";
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.Read();
         }
     }
 }
---
+++ IndexersClasses/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace IndexersClasses
+{
+    public class Names
+
+    {
+        private string[] names;
+        public int Count
+        {
+           get
+            {
+               return names.Length;
+            }
+        }
+
+        public Names(int numberOfValues)
+        {
+           names = new string[numberOfValues];
+        }
+
+        public string this[int i]
+        {
+           get
+            {
+                if (i < 0)
+                    throw new IndexOutOfRangeException("Index musi być większy lub równy zero");
+                return names[i];
+            }
+            set
+            {
+                if (i < 0)
+                    throw new IndexOutOfRangeException("Index musi być większy lub równy zero");
+                names[i] = value;
+            }
+        }
+    }
+}
